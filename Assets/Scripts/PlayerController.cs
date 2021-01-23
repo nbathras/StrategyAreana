e000885f6d4b1213f93ff4206b4f7ecc796dec00
Noah Bathras <nbathras@terpmail.umd.edu>
2021-01-23 17:44:35 -0500

    Added URP and NetworkManager and PlayerController movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour {
+
+    [SerializeField] private float speed;
+
+    private void Update() {
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
+        if (isLocalPlayer) {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(
+                moveHorizontal * speed,
+                moveVertical * speed,
+                0
+            );
+            transform.position += movement * Time.deltaTime;
+        }
+    }
+}
